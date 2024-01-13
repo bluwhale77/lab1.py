edab5f99e6b2f2067b7a2379298111af294af66c
bluwhale77 <156498588+bluwhale77@users.noreply.github.com>
2024-01-12 22:57:51 -0800

    Update lab1.py
    Miscellaneous assignment instructions removed

--- lab1.py
+++ lab1.py
@@ -1,9 +1,7 @@
 # lab1.py
 
 # Starter code for lab 1 in ICS 32 Programming with Software Libraries in Python
-# Replace the following placeholders with your information.
-# Please see the README in this repository for the requirements of this lab
-exercise
+# Please see the README in this repository for the requirements of this lab exercise
 
 # Bryant Lu
 # Lubg@uci.edu
