df74a85c53c8b3db93eb9256b85c1d586cc6e56e
bluwhale77 <156498588+bluwhale77@users.noreply.github.com>
2024-01-12 22:50:53 -0800

    Create lab1.py
    Creation of code for lab1.py including starter python file given on Canvas

---
+++ lab1.py
@@ -0,0 +1,29 @@
+# lab1.py
+
+# Starter code for lab 1 in ICS 32 Programming with Software Libraries in Python
+# Replace the following placeholders with your information.
+# Please see the README in this repository for the requirements of this lab
+exercise
+
+# Bryant Lu
+# Lubg@uci.edu
+# 83665984
+
+print("Welcome to ICS 32 PyCalc!")
+
+print()
+
+operand1 = int(input("Enter your first operand: "))
+operand2 = int(input("Enter your second operand: "))
+operator = input("Enter your desired operator (+, -, or x): ")
+
+print()
+
+if operator == '+':
+    answer = operand1 + operand2
+elif operator == '-':
+    answer = operand1 - operand2
+elif operator == 'x':
+    answer = operand1 * operand2
+    
+print(f'The result of your calculation is: {answer}')
